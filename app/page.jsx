97ad2f7c532dd83dc8e1ba46517f0ed5dfd5cb52
deepbhakare <136368452+deepbhakare@users.noreply.github.com>
2023-12-01 22:33:29 +0530

    add styling

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <h1 class="text-white-600 text-5xl font-bold">LOGIN AUTHENTICATION WITH CLERK </h1>
     </>
   );
 }
