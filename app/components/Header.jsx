bae650a394bdd076e41ffb0f35a17f108351499e
deepbhakare <136368452+deepbhakare@users.noreply.github.com>
2023-12-01 22:53:46 +0530

    add dashnboard page

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -18,13 +18,13 @@ const Header = () => {
           {!userId && (
             <>
               <Link
-                href="Sign-in"
+                href="sign-in"
                 className="text-gray-300 hover:text-white mr-4"
               >
                 Sign In
               </Link>
               <Link
-                href="Sign-up"
+                href="sign-up"
                 className="text-gray-300 hover:text-white mr-4"
               >
                 Sign Up
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -4,7 +4,7 @@ const DashboardPage = () => {
   return (
     <>
       <h1 className="text-2xl font-bold mb-5">DashBoard</h1>
-      <p className="mb-5">Welcome to the dash</p>
+      <p className="mb-5 ">Welcome to the dash</p>
     </>
   );
 };
